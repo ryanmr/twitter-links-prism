0b64d3f35f1ae784d2c0a7a76049ae337ce90a91
vagrant <vagrant@homestead>
2016-04-10 16:04:22 +0000

    Added faker factory for Tweets

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,18 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Tweet::class, function(Faker\Generator $faker) {
+  $tweet_id = $faker->randomNumber(5);
+  return [
+    'tweet_id' => $tweet_id,
+    'tweet_url' => 'https://twitter.com/ryanmr/status/' . $tweet_id,
+    'embedded_url' => $faker->url,
+    'display_url' => $faker->domainName,
+    'remote_title' => $faker->slug,
+    'remote_description' => $faker->sentence,
+    'retweet_count' => $faker->randomDigit,
+    'favorite_count' => $faker->randomDigit,
+    'tweet_created_at' => $faker->iso8601
+  ];
+});
